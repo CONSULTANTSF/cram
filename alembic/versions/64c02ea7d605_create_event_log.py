a60528710bd805f5b5fec714347c3272839a8cd2
Alyssa Kwan <alyssa@qbizinc.com>
2019-01-07 11:15:10 -0800

    alembic

---
+++ alembic/versions/64c02ea7d605_create_event_log.py
@@ -0,0 +1,24 @@
+"""create event log
+
+Revision ID: 64c02ea7d605
+Revises: 
+Create Date: 2019-01-07 11:14:03.213058
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '64c02ea7d605'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
